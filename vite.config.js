7dce268ef2f8a943ade8e565196886e01a836ef9
forst09 <dforst09@gmail.com>
2024-08-02 19:25:31 +0300

    add desctop for main screen

---
+++ vite.config.js
@@ -0,0 +1,40 @@
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+import handlebars from 'vite-plugin-handlebars';
+import autoprefixer from 'autoprefixer';
+import path from 'path';
+import sortMediaQueries from 'postcss-sort-media-queries';
+import legacy from '@vitejs/plugin-legacy';
+
+export default defineConfig(() => {
+    return {
+        base: '',
+        plugins: [
+            ViteImageOptimizer({
+                jpg: {
+                    quality: 80
+                },
+                jpeg: {
+                    quality: 80
+                },
+            }),
+            handlebars({
+                partialDirectory: resolve(__dirname, 'src/layout/'),
+            }),
+            legacy({
+                targets: ['defaults', 'not IE 11'],
+            }),
+        ],
+        css: {
+            postcss: {
+                plugins: [
+                    autoprefixer(),
+                    sortMediaQueries({
+                        sort: 'desktop-first'
+                    })
+                ]
+            }
+        }
+    };
+});
